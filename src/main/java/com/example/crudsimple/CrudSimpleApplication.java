1aa1a4ab312b934b3478cdb0f8568e3b35433a05
Higinio01 <HRSP0001@ce.pucmm.edu.do>
2023-10-01 16:15:19 -0400

    Agregar comentario de enlace de uso

--- src/main/java/com/example/crudsimple/CrudSimpleApplication.java
+++ src/main/java/com/example/crudsimple/CrudSimpleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudSimpleApplication {
+    //Usar esta ruta: localhost:8080/estudiantes/listar
 
     public static void main(String[] args) {
         SpringApplication.run(CrudSimpleApplication.class, args);
